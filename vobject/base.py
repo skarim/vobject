b79a562f91fec38816aab4371d1343042b2c2403
Tim Baxter <tim.baxter@cerner.com>
2014-12-29 15:56:56 -0600

    this is dumb

--- vobject/base.py
+++ vobject/base.py
@@ -614,7 +614,7 @@ class Component(VBase):
 
     def __str__(self):
         if self.name:
-            return "<%s| %s>" % (six.u(self.name), ''.join(six.u(e) for e in self.getSortedChildren()))
+            return "<%s| %s>" % (six.u(self.name), ''.join(self.getSortedChildren()))
         else:
             return u'<*unnamed*| {}>'.format(self.getSortedChildren())
 
