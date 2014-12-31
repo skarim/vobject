a48207ef5adc20359b9558eade3eb100df657d3c
Tim Baxter <tim.baxter@cerner.com>
2014-12-31 11:41:16 -0600

    cleanup on tests

---
+++ vobject/tests.py
@@ -0,0 +1,43 @@
+import unittest
+
+from six import StringIO
+
+from vobject.base import readComponents
+
+
+def get_test_file(path):
+    """
+    Helper function to open and read test files.
+    """
+    filepath = "%stest_files/%s" % (__name__, path)
+    f = open(filepath, 'r').read()
+    return f
+
+
+class TestVobject(unittest.TestCase):
+
+    def setUp(self):
+
+        self.simple_test_cal = get_test_file("simple_test.ics")
+
+    def test_readComponents(self):
+        f = StringIO(self.simple_test_cal)
+        cal = next(readComponents(f))
+
+        self.assertEqual(cal, "<VCALENDAR| [<VEVENT| [<SUMMARY{u'BLAH': [u'hi!']}Bastille Day Party>]>]>")
+        self.assertEqual(cal.vevent.summary, "<SUMMARY{u'BLAH': [u'hi!']}Bastille Day Party>")
+
+
+    """def test_choice(self):
+        element = random.choice(self.seq)
+        self.assertTrue(element in self.seq)
+
+    def test_sample(self):
+        with self.assertRaises(ValueError):
+            random.sample(self.seq, 20)
+        for element in random.sample(self.seq, 5):
+            self.assertTrue(element in self.seq)
+    """
+
+if __name__ == '__main__':
+    unittest.main()
