c0aad5c7ef7c016f78dcbd3132580375839694b7
Will Percival <wpercy27@gmail.com>
2016-12-06 13:49:01 -0500

    move error logging outside of if/else

--- vobject/base.py
+++ vobject/base.py
@@ -1076,10 +1076,9 @@ def readComponents(streamOrString, validate=False, transform=True,
                 except VObjectError as e:
                     if e.lineNumber is not None:
                         msg = "Skipped line {lineNumber}, message: {msg}"
-                        logger.error(msg.format(**{'lineNumber': e.lineNumber, 'msg': str(e)}))
                     else:
-                        msg = "Skipped a line, message: {0}"
-                        logger.error(msg.format(str(e)))
+                        msg = "Skipped a line, message: {msg}"
+                    logger.error(msg.format(**{'lineNumber': e.lineNumber, 'msg': str(e)}))
                     continue
             else:
                 vline = textLineToContentLine(line, n)
